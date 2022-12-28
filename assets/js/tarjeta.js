22e11d37eef0e3243c3cf7e9dd38dc742d6a56e4
nahuescudaletti <nahueby17@gmail.com>
2022-12-28 20:32:50 -0300

    second commit

---
+++ assets/js/tarjeta.js
@@ -0,0 +1,7 @@
+VanillaTilt.init(document.querySelectorAll(".box"), {
+    max: 25,
+    speed: 300,
+    easing: "cubic-bezier(.03,.98,.52,.99)",
+    perspective: 500,
+    transition: true
+  });
